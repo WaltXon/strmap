be11fd86d7f28bbeede984b4414ef39f90bc3d93
Walt <walt@waltnixon.com>
2014-04-02 15:02:05 -0500

    parseSTR() and parsePart() working

--- strparse.py
+++ strparse.py
@@ -1,7 +1,33 @@
 
-test = ['NE4', 'SE4', 'NENE', 'S2S2', 'S2', 
+test = ['NE4', 'SE4', 'NENE', 'S2S2', 'S2',
 	'N2S2+W2NW4', 'ALL','SW4NE4','S2NW4+SW4','E2', 'W2W2']
 
+
+def parsePart(part):
+	if part == 'ALL':
+		print 'Unmodified Section'
+		return(["ALL"])
+	else:
+		partstr = ''
+		parts = []
+		for char in part:
+			if partstr == '':
+				if char in ('E','W'):
+					parts.append(char)
+				elif char in ('N','S'):
+					partstr += char
+			else:
+				if char in ('N', 'S'):
+					parts.append(partstr)
+					partstr = char
+				elif char in ('E', 'W'):
+					partstr += char
+					parts.append(partstr)
+					partstr = ''
+
+		if partstr != '':
+			parts.append(partstr)
+	return parts
 
 
 def parseSTR(legal):
@@ -10,55 +36,24 @@ def parseSTR(legal):
 	numeric = ['2','4']
 	section = 640.0 #get area of shape in actual script
 
-	print '== {0}'.format(legal)
+	#remove the numers from the string
+	print 'Original == {0}'.format(legal)
 	legal = legal.replace('2', '')
 	legal = legal.replace('4', '')
-	print '-- {0}'.format(legal)
-	parts = []
-	nparts = []
+	# print '-- {0}'.format(legal)
 
-	if legal == 'ALL':
-		print 'Unmodified Section = {0}'.format(section)
-		parts.append("ALL")
-		nparts.append(parts)
-	else:
-		charidx = 0
-		partidx = 0
-		part = ''
-		
-		for char in legal:
-			if char == '+':
-				print 'Multipart Description'
-				charidx = 0
-				partidx += 1
-			
-			if charidx == 0:
-				if char in ('E','W'):
-					part += char
-					print '{0} Half'.format(part)
-					parts.append(part)
-					nparts.append(parts)
-					part = ''
-					parts = []
-				elif char in ('N','S'):
-					part += char
-					charidx = 1
-			elif charidx > 0:
-				if char in ('E', 'W'):
-					part += char
-					parts.append(part)
-					part = ''
-					charidx = 0
-				else:
-					parts.append(part)
-					part = ''
-					charidx = 0
+	partlist = legal.split("+")
+	print("partlist = {}".format(partlist))
 
-	print parts
-	print nparts
+	legallist = []
+	for part in partlist:
+		print("--part = {0}".format(part))
+		legallist.append(parsePart(part))
 
-
-
+	return legallist
 
 for legal in test:
-	parseSTR(legal)
+	print("""
+	      =================
+	      """)
+	print("ParsedSTR = {0}".format(parseSTR(legal)))
